67ed288591131e166d7de3764ea9f9220dfcd375
eipeks <apexzdgr8est@gmail.com>
2017-10-06 18:55:28 +0100

    Initial commit

---
+++ app/src/main/java/i/am/eipeks/projectcrypto/Home.java
@@ -0,0 +1,13 @@
+package i.am.eipeks.projectcrypto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
