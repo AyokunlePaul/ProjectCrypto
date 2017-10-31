7f73b247c94041aba9fe6ac0b7759dc1fc16798d
eipeks <apexzdgr8est@gmail.com>
2017-10-31 11:59:27 +0100

    Added required dependencies

---
+++ app/src/main/java/i/am/eipeks/projectcrypto/_utils/NetworkUtils.java
@@ -0,0 +1,16 @@
+package i.am.eipeks.projectcrypto._utils;
+
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+
+    @SuppressWarnings("ConstantConditions")
+    public static boolean isPhoneConnected(Context context){
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return info != null && info.isConnected();
+    }
+}
--- app/src/main/java/i/am/eipeks/projectcrypto/_view/Home.java
+++ app/src/main/java/i/am/eipeks/projectcrypto/_view/Home.java
@@ -1,7 +1,9 @@
-package i.am.eipeks.projectcrypto;
+package i.am.eipeks.projectcrypto._view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import i.am.eipeks.projectcrypto.R;
 
 public class Home extends AppCompatActivity {
 
